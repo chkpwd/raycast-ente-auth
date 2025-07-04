67cf8e03218fbe8af3b2b3a9f2d32bc106155d81
Nico-Mayer <nico.m0623@gmail.com>
2025-07-04 19:16:14 +0200

    chore(optional): add prettier in favor of editorconfig, because its more reliable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,12 +18,15 @@
 - Restructured imports and added logging when folders already exist
 
 ## [Bug fix] - 2024-11-14
+
 - Allow path to be resolved during runtime (e.g ~/Desktop/Ente)
 
 ## [Update Readme] - 2024-11-14
+
 - Fix a typo in the readme
 
 ## [Bug fix] - 2024-11-13
+
 - Added PR-15036 changes contributed by albarin
 
 ## [New Additions] - 2024-11-13
@@ -31,6 +34,7 @@
 - Add the ability to change preferred actions; e.g. (paste or copy on enter)
 
 ## [Bug fix] - 2024-08-28
+
 - Fix error in `Get TOPT` command when secrets are empty
 
 ## [Initial Version] - 2024-08-28
--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -8,38 +8,37 @@
 /* eslint-disable @typescript-eslint/ban-types */
 
 type ExtensionPreferences = {
-  /** Ente CLI Path - If you wish to use your own CLI, type here the location of your local installation. */
-  "cliPath": string,
-  /** Ente CLI Export Location - If you wish to change the default export path, type here the location of your local installation. */
-  "exportPath"?: string,
-  /** Primary Action - Choose whether the primary action should copy or paste the output. */
-  "primaryAction": "copy" | "paste",
-  /** Privacy - Choose whether the export should be deleted after import. */
-  "deleteExport"?: boolean
-}
+	/** Ente CLI Path - If you wish to use your own CLI, type here the location of your local installation. */
+	cliPath: string;
+	/** Ente CLI Export Location - If you wish to change the default export path, type here the location of your local installation. */
+	exportPath?: string;
+	/** Primary Action - Choose whether the primary action should copy or paste the output. */
+	primaryAction: "copy" | "paste";
+	/** Privacy - Choose whether the export should be deleted after import. */
+	deleteExport?: boolean;
+};
 
 /** Preferences accessible in all the extension's commands */
-declare type Preferences = ExtensionPreferences
+declare type Preferences = ExtensionPreferences;
 
 declare namespace Preferences {
-  /** Preferences accessible in the `index` command */
-  export type Index = ExtensionPreferences & {}
-  /** Preferences accessible in the `export` command */
-  export type Export = ExtensionPreferences & {}
-  /** Preferences accessible in the `import` command */
-  export type Import = ExtensionPreferences & {}
-  /** Preferences accessible in the `delete` command */
-  export type Delete = ExtensionPreferences & {}
+	/** Preferences accessible in the `index` command */
+	export type Index = ExtensionPreferences & {};
+	/** Preferences accessible in the `export` command */
+	export type Export = ExtensionPreferences & {};
+	/** Preferences accessible in the `import` command */
+	export type Import = ExtensionPreferences & {};
+	/** Preferences accessible in the `delete` command */
+	export type Delete = ExtensionPreferences & {};
 }
 
 declare namespace Arguments {
-  /** Arguments passed to the `index` command */
-  export type Index = {}
-  /** Arguments passed to the `export` command */
-  export type Export = {}
-  /** Arguments passed to the `import` command */
-  export type Import = {}
-  /** Arguments passed to the `delete` command */
-  export type Delete = {}
+	/** Arguments passed to the `index` command */
+	export type Index = {};
+	/** Arguments passed to the `export` command */
+	export type Export = {};
+	/** Arguments passed to the `import` command */
+	export type Import = {};
+	/** Arguments passed to the `delete` command */
+	export type Delete = {};
 }
-
